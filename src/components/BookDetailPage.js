afe0ca1a9c11a5394830f28ec0d664deae3be343
Benoit <benoitredondo@gmail.com>
2021-04-15 11:54:06 +0200

    Header Grid completed

---
+++ src/components/BookDetailPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './bookDetailPage.css';
+
+const title = 'Les aventures de Benoît';
+const authorName = 'Benoît REDONDO';
+const averageNote = '🤩🤩🤩';
+// eslint-disable-next-line prettier/prettier
+const cloudWord =
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias corporis enim deleniti possimus dolorem facilis tenetur porroexercitationem  ipsa eligendi velit reprehenderit. Cumque rem, quos sint doloribus voluptas ex delectus';
+const bookCover = 'https://i.ibb.co/tpj91z3/9782265144286-475x500-1.webp';
+
+//* *Je créée ma fonction :
+
+function BookDetailPage() {
+  return (
+    <>
+      <div id="wrapHeader">
+        <img src={bookCover} alt="première de couverture :{title} " />
+        <h1 id="title">{title}</h1>
+        <h1 id="authorName">{authorName}</h1>
+        <h1 id="averageNote">{averageNote}</h1>
+        <h1 id="cloudWord">{cloudWord}</h1>
+      </div>
+
+      <div id="wrapMain">
+        {' '}
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias,
+        corporis enim deleniti possimus dolorem facilis tenetur porro
+        exercitationem ipsa eligendi velit reprehenderit. Cumque rem, quos sint
+        doloribus voluptas ex delectus.{' '}
+      </div>
+
+      <div id="wrapFooter">
+        <div id="media">Media</div>
+        <div id="place">Where all scenes took place</div>
+        <div id="specification">Details about the book</div>
+      </div>
+    </>
+  );
+}
+
+export default BookDetailPage;
