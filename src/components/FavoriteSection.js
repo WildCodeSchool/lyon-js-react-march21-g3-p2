d44a45502f59055ceff45da37f8de92bd746f8eb
PSchnur <prisciliaschnur@outlook.fr>
2021-04-16 08:33:42 +0200

    skeleton and layout home page

---
+++ src/components/FavoriteSection.js
@@ -0,0 +1,9 @@
+import './FavoriteSection.css';
+
+export default function FavoriteSection() {
+  return (
+    <div className="favoriteContainer">
+      <h1> COUPS DE COEUR</h1>
+    </div>
+  );
+}
---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeroSection.css';
+
+export default function HeroSection() {
+  return (
+    <div>
+      <div className="heroContainer"> </div>
+      <div className="catchphraseContainer">
+        <h1>Phrase d'accroche</h1>
+      </div>
+    </div>
+  );
+}
