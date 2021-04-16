c7ab05662c516fc99f75a2db9b3225077456409d
Ornella Gatto <ornellagatto7@gmail.com>
2021-04-16 12:43:27 +0200

    header ok

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,8 @@
-const message = "Thank you for submitting your request.";
+const message = 'Thank you for submitting your request.';
 
 document
-    .getElementById("contactForm")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert(message);
-    });
+  .getElementById('contactForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert(message);
+  });
