4d17015552f4f225756ee8fffbdd11c6366b1061
Benoit <benoitredondo@gmail.com>
2021-04-19 16:51:04 +0200

    completed grid with responsive design

--- src/components/BookDetailPage.js
+++ src/components/BookDetailPage.js
@@ -7,22 +7,18 @@ const averageNote = '🤩🤩🤩';
 // eslint-disable-next-line prettier/prettier
 const cloudWord =
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias corporis enim deleniti possimus dolorem facilis tenetur porroexercitationem  ipsa eligendi velit reprehenderit. Cumque rem, quos sint doloribus voluptas ex delectus';
-const bookCover = 'https://i.ibb.co/tpj91z3/9782265144286-475x500-1.webp';
 
 //* *Je créée ma fonction :
 
 function BookDetailPage() {
   return (
     <>
-      <div id="wrapHeader">
-        <img src={bookCover} alt="première de couverture :{title} " />
-        <h1 id="title">{title}</h1>
-        <h1 id="authorName">{authorName}</h1>
+      <div id="wrap">
+        <div id="coverbook">image</div>
+        <h2 id="title">{title}</h2>
+        <h2 id="authorName">{authorName}</h2>
         <h1 id="averageNote">{averageNote}</h1>
-        <h1 id="description">{cloudWord}</h1>
-      </div>
-
-      <div id="wrapFooter">
+        <h3 id="description">{cloudWord}</h3>
         <div id="media">Media</div>
         <div id="place">Where all scenes took place</div>
         <div id="specification">Details about the book</div>
