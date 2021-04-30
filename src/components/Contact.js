df6b3558627d0de4aa33b59f4082e18026e44c4c
Benoit <benoitredondo@gmail.com>
2021-04-30 11:08:57 +0200

    finish contact-us with CSS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import './contact-us.css';
+import { TextField } from '@material-ui/core';
 
 export default function Form() {
   const {
     register,
-    handleSubmit,
     reset,
+    handleSubmit,
     formState: { errors },
   } = useForm();
 
@@ -15,55 +17,44 @@ export default function Form() {
   console.log(errors);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="prenom"> Votre prénom : </label>
-      <input
-        type="text"
-        placeholder="Prénom"
-        {...register('prenom', {
-          required: 'this is required',
-          maxLength: 80,
-          pattern: /([A-Z])\w+/i,
-        })}
-      />
-      {errors.prenom && 'First name is required'}
+    <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <div id="name">
+        <TextField
+          id="outlined-basic1"
+          label="Prénom"
+          variant="outlined"
+          {...register('prenom', { required: true })}
+        />
+        {errors.firstName && 'Surname is required'}
+        <br />
+        <TextField
+          id="outlined-basic2"
+          label="Nom"
+          variant="outlined"
+          {...register('Nom', { required: true })}
+        />
+        <br />
+        <TextField
+          id="outlined-basic3"
+          label="Numéro de téléphone"
+          variant="outlined"
+          {...register('number', { required: true })}
+        />
+      </div>
       <br />
-      <label htmlFor="nom"> Votre nom: </label>
-      <input
-        type="text"
-        placeholder="Nom"
-        {...register('nom', {
-          required: true,
-          maxLength: 80,
-          pattern: /([A-Z])\w+/i,
-        })}
-      />
-      {errors.nom && 'Name is required'}
-      {reset}
-      <br />
-      <label htmlFor="email"> Votre email: </label>
-      <input
-        type="email"
-        placeholder="Email"
-        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      {errors.email && 'Email is required'}
-      {reset}
-      <br />
-      <label htmlFor="email"> Votre numéro de téléphone: </label>
-      <input
-        type="tel"
-        placeholder="Mobile number"
-        {...register('mobileNumber', { required: true, maxLength: 12 })}
+      <TextField
+        id="outlined-basic4"
+        label="Commentaire"
+        variant="outlined"
+        {...register('comment', { required: true })}
       />
       <br />
-      <label htmlFor="question"> Votre demande : </label>
-      <textarea
-        placeholder="Quelle est votre question ?"
-        {...register('question', { required: true })}
+      <input
+        style={{ marginTop: 20 }}
+        type="button"
+        onClick={() => reset()}
+        value="Envoyer ma demande"
       />
-      {errors.question && 'A message is required'}
-      <input type="submit" onClick={() => reset()} />
     </form>
   );
 }
